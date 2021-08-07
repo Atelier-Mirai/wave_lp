9e8993a168b6ee60e912c757742e7180e0dcdac1
Atelier-Mirai <contact@atelier-mirai.net>
2021-08-07 19:44:23 +0900

    モバイルメニュー 実装完了

--- navigation.js
+++ navigation.js
@@ -3,23 +3,23 @@ const fixedAnimationForDesktop = () => {
   // スクロール量を取得
   let scroll       = $(window).scrollTop();
 
-  // 200px以上、スクロールしたら
-  if (scroll >= 200) {
+  // 300px以上、スクロールしたら
+  if (scroll >= 300) {
     // ヘッダーを表示
-    $("#header").removeClass("hidden");
-    $("#header").addClass("shown");
+    $("#desktop-header").removeClass("hidden");
+    $("#desktop-header").addClass("shown");
     // 上から現れるよう、動きのためのクラス付与
-    $("#header").removeClass("upward");
-    $("#header").addClass("downward");
+    $("#desktop-header").removeClass("upward");
+    $("#desktop-header").addClass("downward");
   }
   // そうでなければ
   else {
     // 画面上部に消えていく動きのためのクラス付与
-    $("#header").removeClass("downward");
-    $("#header").addClass("upward");
+    $("#desktop-header").removeClass("downward");
+    $("#desktop-header").addClass("upward");
     // ヘッダーを非表示
-    $("#header").removeClass("shown");
-    $("#header").addClass("hidden");
+    $("#desktop-header").removeClass("shown");
+    $("#desktop-header").addClass("hidden");
   }
 }
 
@@ -28,36 +28,37 @@ const fixedAnimationForMobile = () => {
   // スクロール量を取得
   let scroll       = $(window).scrollTop();
 
-  // 200px以上、スクロールしたら
-  if (scroll >= 200) {
+  // 300px以上、スクロールしたら
+  if (scroll >= 300) {
     $("#mobile-header").removeClass("hidden");
     $(".open_button").addClass("fadeDown");
-    $("#mobile-header").addClass("done");
   }
   // そうでなければ
   else {
     $("#mobile-header").addClass("hidden");
     $(".open_button").removeClass("fadeDown");
-    $("#mobile-header").removeClass("done");
   }
 }
 
 // 画面幅を取得
 let windowWidth = $(window).width();
 
-// 画面がスクロールされたら、アニメーション関数を呼び出す
+// 画面がスクロールときに、メニュー表示を行う
 if (windowWidth <= 768) {
+  // モバイル版のメニュー表示
   $(window).scroll(fixedAnimationForMobile);
   // ハンバーガーボタンをクリックした際の処理
   $(".open_button").on("click", () => {
     $(".open_button").toggleClass("active");
-    $("#mobile-header").toggleClass("panel_active");
+    $("#mobile-menu").toggleClass("active");
   });
-
-  $("#mobile-header nav ul li a").on("click", () => {
+  // リンクをクリックした際の処理
+  $("#mobile-menu li a").on("click", () => {
     $(".open_button").removeClass("active");
-    $("#mobile-header").removeClass("panel_active");
+    $("#mobile-menu").removeClass("active");
+    $(".open_button").removeClass("fadeDown");
   });
 } else {
+  // デスクトップ版のメニュー表示
   $(window).scroll(fixedAnimationForDesktop);
 }
