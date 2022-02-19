33406721327a6cd29f94dd5815c582d87ce82590
Atelier-Mirai <contact@atelier-mirai.net>
2022-02-19 18:13:40 +0900

    PWA 更新

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+/*=========================================================================
+  サービスワーカーの登録を行う
+  // https://jam25.jp/javascript/about-pwa/
+  // https://laboradian.com/create-offline-site-using-sw/
+=========================================================================*/
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(() => {
-      console.log('Service worker registered!');
+  navigator.serviceWorker.register('sw.js')
+    .then((registration) => {
+      // 登録成功
+      console.log(`Service Worker の登録に成功しました。スコープ: ${registration.scope}`);
+    }).catch((error) => {
+      // 登録失敗
+      console.log(`Service Worker の登録に失敗しました。${error}`);
     });
 }
--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
+/*=========================================================================
+  オフラインでも使えるよう、キャッシュを取得し、レスポンスする。
+  // https://jam25.jp/javascript/about-pwa/
+  // https://laboradian.com/create-offline-site-using-sw/
+=========================================================================*/
 const CACHE_DYNAMIC_VERSION = 'dynamic-v1';
-
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   console.log('[Service Worker] Fetching something ...');
   event.respondWith(
     // キャッシュの存在チェック
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_VERSION)
-                .then(function(cache) {
-                  // 最後に res を返せるように、ここでは clone() する必要がある
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function() {
-              // エラーが発生しても何もしない
-            });
-        }
-      })
-  );
+    caches.match(event.request).then((response) => {
+      // キャッシュ内に該当レスポンスがあれば、それを返す
+      if (response) {
+        return response;
+      } else {
+        // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
+        return fetch(event.request).then((res) => {
+          return caches.open(CACHE_DYNAMIC_VERSION).then((cache) => {
+            // 最後に res を返せるように、ここでは clone() する必要がある
+            cache.put(event.request.url, res.clone());
+            return res;
+          })
+        }).catch(() => {
+          // エラーが発生しても何もしない
+        });
+      }
+    }));
 });
