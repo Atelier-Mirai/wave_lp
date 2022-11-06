cb8b75e3e92492a5f66dabcfa75fef172a70b10e
Atelier Mirai <contact@atelier-mirai.net>
2022-11-06 19:37:40 +0900

    微修正

--- sakura.js
+++ sakura.js
@@ -2,7 +2,7 @@
  * 🌸の花びらが舞い散るJavaScript
  * https://actyway.com/8351 を元に作成
  *
- * クラスを使わず、配列達で頑張って処理
+ * オブジェクト指向らしくクラス作成して処理
 =============================================================================*/
 
 (
@@ -12,8 +12,8 @@
   // =========================================================================
   const NUMBER_OF_HANABIRAS = 50; // 花びらの枚数
   const FPS                 = 24; // 一秒間に24回 動かす
-  const HANABIRA_HEIGHT     = 30; // 花びらの高さ 回転するので最大値は 29px
-  const HANABIRA_WIDTH      = 30; // 花びらの幅 回転するので最大値は 29px
+  const HANABIRA_HEIGHT     = 30; // 花びらの高さ 回転するので最大値は 30px
+  const HANABIRA_WIDTH      = 30; // 花びらの幅 回転するので最大値は 30px
   const HANABIRA_Z_BASE     = 10000; // 花びらの z-index の基準値
 
   // ウィンドウの高さ
@@ -22,7 +22,6 @@
   const windowWidth  = document.documentElement.clientWidth
   // スクロール位置
   let scroll         = document.documentElement.scrollTop || document.body.scrollTop;
-
   // スクロール時のイベント登録 (スクロール時に花びらがウィンドウ内に納まる為に)
   document.addEventListener('scroll', () => {
     scroll = document.documentElement.scrollTop || document.body.scrollTop;
@@ -42,51 +41,137 @@
   };
 
   // =========================================================================
+  // 花びらクラスの宣言
+  // =========================================================================
+  class Hanabira {
+    // コンストラクタ(構築子)
+    constructor(id, x, y, z, tremorMax, fallingSpeed, className) {
+      this.id           = id;
+      this.x            = x;
+      this.y            = y;
+      this.z            = z;
+      this.tremorMax    = tremorMax;
+      this.fallingSpeed = fallingSpeed;
+      this.className    = className;
+
+      this.direction    = "right";
+      this.tremorCount  = 0;
+    }
+
+    // 際大揺らぎ回数に達しているか？
+    isTremorMax() {
+      return (this.tremorCount === this.tremorMax)
+    }
+
+    // 揺らぎ方向転換
+    directionSwitch() {
+      if (this.direction === "right") {
+        this.direction = "left";
+      } else {
+        this.direction = "right";
+      }
+    }
+
+    // 花びらの位置に関して
+    // 空中にいるか？(ウィンドウ内か？)
+    isInTheAir() {
+      return (this.y < scroll + windowHeight - HANABIRA_HEIGHT);
+    }
+
+    // 地面に着いたか？
+    isOnTheGround() {
+      return !this.isInTheAir();
+    }
+
+    // 右端にいるか？
+    isOnTheRightEdge() {
+      return (this.x + HANABIRA_WIDTH >= windowWidth);
+    }
+
+    // 左端にいるか？
+    isOnTheLeftEdge() {
+      // 花びら幅の半分の位置なら、左端と見做す。
+      return (this.x <= HANABIRA_WIDTH / 2);
+    }
+
+    // 花びらの x, y 座標を更新する
+    move() {
+      // 花びらの位置がウィンドウ内なら
+      if (this.isInTheAir()) {
+        // 同一方向へtremorMax回移動したなら、移動方向を反転させる
+        if (this.isTremorMax()) {
+          this.directionSwitch();
+          this.tremorCount = 0;
+        }
+
+        // 左右に移動する
+        let deltaX   = rand(0.3, 0.6, "float");
+        let signFlag = (this.direction === "right" ? +1 : -1);
+        this.x      += signFlag * deltaX;
+
+        // もし右端にいるなら、左端に移動する
+        if (this.isOnTheRightEdge()) {
+          this.x = HANABIRA_WIDTH / 2;
+        }
+
+        // もし左端にいるなら、右端に移動する
+        if (this.isOnTheLeftEdge()) {
+          this.x = windowWidth - HANABIRA_WIDTH;
+        }
+
+        // 移動回数を増やす
+        this.tremorCount++;
+
+        // 落下速度分を加える
+        this.y += this.fallingSpeed;
+
+      // もし地面に着いているなら、上に戻す
+      } else if (this.isOnTheGround()) {
+        this.y = scroll;
+        this.x = rand(0, windowWidth - HANABIRA_WIDTH);
+      }
+    }
+
+    // 位置情報を DOM に反映させる
+    applyPositionInformation(domHanabira) {
+      domHanabira.setAttribute('style', `top: ${this.y}px; left: ${this.x}px; z-index: ${this.z};`);
+    }
+  }
+
+  // =========================================================================
   // 初期化処理
   // 🌸の花びらのための新しい div 要素を作成し、body の末尾に追加
-  // 作業用の 花びら配列達も適宜初期化する
+  // 作業用の 花びらインスタンスも生成する
   // =========================================================================
-
   const divSakura = document.createElement("div");
   document.body.after(divSakura);
 
-  // 変数宣言
-  // -------------------------------------------------------------------------
   let domHanabiras = []; // 花びら要素の配列
   let jsHanabiras  = []; // 花びらjsオブジェクトの配列
-  let jsHanabirasY = []; // 花びら達の X 座標の配列
-  let jsHanabirasX = []; // 花びら達の Y 座標の配列
-  let tremorMax    = []; // 花びらの連続して揺らげる回数の配列
-  let tremorCount  = []; // 花びらの揺いだ累積回数の配列
-  let fallingSpeed = []; // 落下速度の配列
-
-
   // それぞれの花びらについて、位置等の初期設定を行う
-  // -------------------------------------------------------------------------
   for(let i = 0; i < NUMBER_OF_HANABIRAS; i++){
+    let id           = i;
+    let x            = rand(HANABIRA_WIDTH / 2, windowWidth - HANABIRA_WIDTH);
+    let y            = rand(-500, 0) + scroll;
+    let z            = HANABIRA_Z_BASE + i;
+    let tremorMax    = rand(15, 50);
+    let fallingSpeed = rand(1, 3);
+    let className    = `hana t${rand(1, 5)} a${rand(1, 5)}`;
+    let jsHanabira   = new Hanabira(id, x, y, z, tremorMax, fallingSpeed, className);
+    jsHanabiras[i]   = jsHanabira;
+
     // 花びらの div を作る
     let domHanabira = document.createElement('div');
-
-    // ランダムに生成した初期表示位置を設定する
-    jsHanabirasX[i] = rand(HANABIRA_WIDTH / 2, windowWidth - HANABIRA_WIDTH);
-    jsHanabirasY[i] = rand(-500, 0) + scroll;
-    domHanabira.setAttribute('style', `z-index: ${HANABIRA_Z_BASE + i}; top: ${jsHanabirasY[i]}px; left: ${jsHanabirasX[i]}px;`);
-
+    // 初期表示位置を設定する
+    jsHanabira.applyPositionInformation(domHanabira);
+    // ID 付与
+    domHanabira.id = i;
     // ランダムに生成した花びらの色とアニメーションのための css class を設定する
-    domHanabira.setAttribute('class', `hana t${rand(1, 5)} a${rand(1, 5)}`);
-
-    // 扱いやすくするために、花びら要素達を配列に格納
-    domHanabiras[i] = domHanabira;
-
+    domHanabira.setAttribute('class', jsHanabira.className);
     // 作成した花びらをDOMに追加、画面に表示されるようにする
     divSakura.appendChild(domHanabira);
-
-    // 連続して同一方向へ揺らげる回数をランダムに生成
-    tremorMax[i]    = rand(15, 50);
-    // 連続して同一方向へ揺らいだ回数を0で初期化
-    tremorCount[i]  = 0;
-    // 落下速度をランダムに生成
-    fallingSpeed[i] = rand(1, 3);
+    // 扱いやすくするために、花びら要素達を配列に格納
+    domHanabiras[i] = domHanabira;
   }
 
   // =========================================================================
@@ -94,47 +179,14 @@
   // 生成したそれぞれの花びらの位置情報を更新し、画面に反映する。
   // =========================================================================
   setInterval(() => {
-    // 各花びらに対し移動処理を繰り返す
-    for(let i = 0; i < NUMBER_OF_HANABIRAS; i++){
-      // 花びらの位置（top）がウィンドウ内なら
-      if (jsHanabirasY[i] < scroll + windowHeight - HANABIRA_HEIGHT){
-        // 揺らいだ回数が、連続して揺らげる回数以内なら、右へ移動させる
-        if (tremorCount[i] <= tremorMax[i]){
-          jsHanabirasX[i] += rand(0.3, 0.6, "float");
-          // もし、花びらが右にはみ出すようなら、左端に移動させる
-          if (jsHanabirasX[i] + HANABIRA_WIDTH >= windowWidth){
-            jsHanabirasX[i] = HANABIRA_WIDTH;
-          }
-          // 連続して右へ揺らいだならば、今度は左へ移動させる
-        } else {
-          jsHanabirasX[i] -= rand(0.3, 0.6, "float");
-          // もし、花びらが左にはみ出すようなら、右端に移動させる
-          if (jsHanabirasX[i] <= HANABIRA_WIDTH / 2){
-            jsHanabirasX[i] = windowWidth - HANABIRA_WIDTH;
-          }
-        }
+    for(let jsHanabira of jsHanabiras) {
+      // 各花びらに対し、位置情報の更新処理を行う
+      jsHanabira.move();
 
-        // 右へtremorMax回、左へtremorMax回、揺らいだら、累積回数を0に初期化
-        // これにより、再び、右へ揺らげるようにする
-        if (tremorCount[i] >= tremorMax[i] * 2){
-          tremorCount[i] = 0;
-        }
-
-      // 花びらがウィンドウの下まできたら
-      } else {
-        // 花びらをウィンドウの上に戻し、画面幅内に表示する
-        jsHanabirasX[i] = rand(0, windowWidth - HANABIRA_WIDTH);
-        jsHanabirasY[i] = scroll;
-      }
-
-      // 表示位置に落下速度分を追加する
-      jsHanabirasY[i]               += fallingSpeed[i];
-
-      // 花びらの位置を設定する
-      domHanabiras[i].style.left = `${jsHanabirasX[i]}px`;
-      domHanabiras[i].style.top  = `${jsHanabirasY[i]}px`;
-      // 同一方向への移動回数更新
-      tremorCount[i]++;
+      // js オブジェクトの位置情報を、domオブジェクトモデルの位置に反映する。
+      let id          = jsHanabira.id;
+      let domHanabira = domHanabiras[id];
+      jsHanabira.applyPositionInformation(domHanabira);
     }
   }, 1000 / FPS);
 }
